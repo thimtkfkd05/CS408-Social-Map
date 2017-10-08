fe30b8442fd35b9c8b809787a6a55d8a66df523b
thimtkfkd05 <thimtkfkd05@gmail.com>
2017-10-08 20:40:57 +0900

    node express version up to 4

--- app.js
+++ app.js
@@ -4,33 +4,31 @@
  */
 
 var express = require('express')
+  , bodyParser = require('body-parser')
+  , methodOverride = require('method-override')
+  , logger = require('morgan')
+  , errorHandler = require('errorhandler')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.configure(function(){
-  app.set('port', port);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+app.use(errorHandler());
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
