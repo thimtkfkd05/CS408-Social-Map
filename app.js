860d0e0366f48ab645d9cba6531e0f780de7e431
thimtkfkd05 <thimtkfkd05@gmail.com>
2017-11-25 22:06:12 +0900

    added google login

--- app.js
+++ app.js
@@ -52,5 +52,5 @@ function connectDB(){
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 
-  connectDB();
+  //connectDB();
 });
